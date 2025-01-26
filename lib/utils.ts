7345acb10d987f9e0ace12e15e602a667ebdc4f1
Emmanuel-Anaba <emmaanaba29@gmail.com>
2025-01-26 22:56:58 +0100

    Added getIconForCategory utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,17 @@
 import { clsx, type ClassValue } from "clsx";
+import {
+  Heart,
+  BookOpen,
+  Briefcase,
+  Utensils,
+  Moon,
+  Users,
+  Palette,
+  Star,
+  Church,
+} from "lucide-react";
 import { twMerge } from "tailwind-merge";
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -25,4 +37,29 @@ export function formatDate(date: Date) {
     minute: "numeric",
     second: "numeric",
   });
+}
+
+export function getIconForCategory(category: string) {
+  switch (slugify(category)) {
+    case "health":
+    case "fitness":
+      return Heart;
+    case "study":
+      return BookOpen;
+    case "work":
+    case "productivity":
+      return Briefcase;
+    case "nutrition":
+      return Utensils;
+    case "sleep":
+      return Moon;
+    case "social":
+      return Users;
+    case "hobby":
+      return Palette;
+    case "spirituality":
+      return Church;
+    default:
+      return Star;
+  }
 }
