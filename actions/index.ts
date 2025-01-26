cd789f120c2cfd4839126c76434179c58408fba8
Emmanuel-Anaba <emmaanaba29@gmail.com>
2025-01-26 23:34:59 +0100

    Added function to toggle completed on a habit

--- actions/index.ts
+++ actions/index.ts
@@ -1,4 +1,5 @@
 "use client";
+import { calculateStreaks } from "@/lib/utils";
 import { Habit } from "@/types";
 
 const STORAGE_KEY = "daily_habits";
@@ -40,5 +41,18 @@ export function addHabit(title: string, category: string) {
     completedDays: [],
   };
   const updatedHabits: Habit[] = [...getHabits(), newHabit];
+  return updatedHabits;
+}
+
+export function toggleHabit(id: string) {
+  const updatedHabits = getHabits().map((habit) => {
+    if (habit.id !== id) return habit;
+    const today = new Date().toLocaleDateString();
+    const completedDays = habit.completedDays.includes(today)
+      ? habit.completedDays.filter((day) => day !== today)
+      : [...habit.completedDays, today];
+    const { currentStreak, longestStreak } = calculateStreaks(completedDays);
+    return { ...habit, completedDays, currentStreak, longestStreak };
+  });
   return updatedHabits;
 }
