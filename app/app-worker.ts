6e4c15dcc575646c56a048063aa6d1b3522c2aa3
Emmanuel-Anaba <emmaanaba29@gmail.com>
2025-01-27 11:24:28 +0100

    Updated app-worker.ts

--- app/app-worker.ts
+++ app/app-worker.ts
@@ -1,5 +1,6 @@
-import { Serwist } from "serwist";
+import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
+import { Serwist } from "serwist";
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
@@ -13,6 +14,18 @@ const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: defaultCache,
+  fallbacks: {
+    entries: [
+      {
+        url: "/offline",
+        matcher({ request }) {
+          return request.destination === "document";
+        },
+      },
+    ],
+  },
 });
 
 serwist.addEventListeners();
